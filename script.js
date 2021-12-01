f4d4c6eae460f38905852c975cb6f0e0caad3788
prompostavka88@bk.ru <svetlyid@yandex.ru>
2021-12-01 20:26:39 +0300

    dnd

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+// блок переменных и констант   ===================================================
+
 var radio = document.getElementsByName("option");
 let writingForm = document.querySelector(".writingBook");
 let loadingForm = document.querySelector(".loadingBook");
+let file = document.querySelector(".nameOnload");
+let downloadLogin = document.querySelector(".downloadLogin")
+let saveBook = document.querySelector(".saveBook");
+let books = [];
+let favoritebooks = [];
+let descriptionBook = document.querySelector(".descriptionBook");
+let nameBook = document.querySelector(".nameWriting");
+let container = document.querySelector(".container");
+let readBook = document.querySelector(".readBook");
+let contentBook = document.querySelector(".contentsOneBook");
+let editOneBook = document.querySelector(".editBook");
+let editBookArea = document.querySelector(".editBookArea");
+let saveOneBook = document.querySelector(".saveOneBook");
+let dropdownAreaBooks = document.querySelector(".dropdownAreaBooks");
+let oneBook1 = document.querySelectorAll(".oneBook");
+
+
+// получаем данные для рендера с LocalStorage 
+if (localStorage.getItem("books")) {
+    books = JSON.parse(localStorage.getItem("books"))
+}
+
+// здесь должен быть РЕНДЕР
+renderLibrary();
+
+// отображение формы  ==============================================================
 
 function testRadio() {
     if (this.value == "writing") {
@@ -19,16 +47,158 @@ for (let i = 0; i < radio.length; i++) {
     radio[i].onchange = testRadio;
 }
 
+// отправка файла на сервер =========================================================
 
-loadingForm.addEventListener("submit", downloadBook)
+let formData = new FormData();
 
-function downloadBook(event) {
-    event.preventDefault();
-    let formdata = new FormData(loadingForm);
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://apiinterns.osora.ru/");
-    xhr.send(formData);
+loadingForm.onsubmit = async (e) => {
+    formData.append("login", downloadLogin.value);
+    formData.append("file", file.files[0]);
+    e.preventDefault();
+    console.log(downloadLogin.value);
+    let response = await fetch("https://apiinterns.osora.ru/", {
+        method: 'POST',
+        body: formData
+    });
+    let result = await response.json();
+    console.log(result);
 };
 
+// Пишем книгу =============================
+
+saveBook.addEventListener("click", function () {
+    let newBook = {
+        name: nameBook.value,
+        description: descriptionBook.value,
+        write: false
+    }
+    if (newBook.name && newBook.description) { books.push(newBook) } else alert("Добавьте заголовок и(или) содержание книги")
+    localStorage.setItem("books", JSON.stringify(books));
+    renderLibrary();
+
+})
+
+
+// функция рендеринга============================
+
+function renderLibrary() {
+    container.innerHTML = "";
+    books.forEach((item, index) => {
+        let blockOneBook = document.createElement("div");
+        blockOneBook.classList.add("oneBook");
+        blockOneBook.setAttribute("draggable", "true");
+        blockOneBook.setAttribute("data-number", index)
+        blockOneBook.innerHTML = `${item.name}
+                                       <button class="readBook" data-index=${index}>Читать книгу</button>
+                                       <button class="reductBook" data-index=${index}>Редактировать</button>
+                                       <button class="deleteBook" data-index=${index}>Удалить</button>
+                                       <button class ="readEndBook" data-index=${index}>Прочитана</button>`
+        container.append(blockOneBook);
+    }
+    )
+}
+
+// функции обработки событий =====================================================================
+// удаление книги
+function deleteBook(element) {
+    books = books.filter((item, index) => index != element)
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+// чтение книги
+function readesBook(element) {
+    contentBook.innerHTML = books[element].description;
+}
+
+//редактировать книгу
+function editBook(element) {
+    editBookArea.value = books[element].description;
+    saveOneBook.dataset.edit = element;
+    editOneBook.style.display = "block";
+
+}
+
+//сохранить книгу
+function saveEditBook(element) {
+    books[element].description = editBookArea.value;
+    localStorage.setItem("books", JSON.stringify(books));
+    renderLibrary();
+    editOneBook.style.display = "none";
+}
+
+
+// задать обработку событий через делегирование блока со списком книг================================================
+
+document.querySelector(".container").addEventListener("click", function (event) {
+    let target = event.target;
+    let indexBook = target.dataset.index;
+
+    if (target.classList.contains("deleteBook")) {
+        deleteBook(indexBook);
+        renderLibrary();
+    }
+    if (target.classList.contains("readBook")) {
+        readesBook(indexBook);
+    }
+    if (target.classList.contains("reductBook")) {
+        editBook(indexBook);
+    }
+})
+
+// делегирование на блок с сохранением и редактированием
+
+document.querySelector(".editBook").addEventListener("click", function (event) {
+    let target = event.target;
+    let indexBook = target.dataset.edit;
+    if (target.classList.contains("saveOneBook")) {
+        saveEditBook(indexBook);
+    }
+})
+
+// делегирование draganddrop==============================================================
+
+// oneBook1.forEach(item => {
+//     item.addEventListener("dragstart", dragstart)
+//     item.addEventListener("dragend", dragend)
+// })
+
+// function dragstart(event) {
+//     console.log("dragstart");
+// }
+
+
+// function dragend(event) {
+//     console.log("dragend");
+// }
+
+
+document.querySelector(".container").addEventListener("dragstart", handleDragStart);
+function handleDragStart(event) {
+    event.dataTransfer.setData("num", event.target.dataset.number)
+    console.log("dragstart");
+}
+
+document.querySelector(".container").addEventListener("dragend", handleDragEnd);
+function handleDragEnd(event) {
+    event.dataTransfer.setData("num", event.target.dataset.number)
+    console.log("dragEnd");
+}
+
+
+document.querySelector(".dropdownarea").addEventListener("dragenter", handleDragEnter);
+function handleDragEnter(event) {
+    event.preventDefault()
+}
+
+document.querySelector(".dropdownarea").addEventListener("dragleave", handleDragLeave);
+function handleDragLeave(event) {
+    event.preventDefault()
+}
+
+
+document.querySelector(".dropdownarea").addEventListener("dragover", handleDragOver);
+function handleDragOver(event) {
+    event.preventDefault()
+}
 
 
